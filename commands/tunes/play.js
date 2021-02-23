70f48f190a2a942adc3b674c8d760f2fc897c701
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-02-23 05:59:05 -0500

    Update play.js

--- commands/tunes/play.js
+++ commands/tunes/play.js
@@ -25,7 +25,7 @@ class PlayCommand extends Command {
 		if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - You are not in the same voice channel !`);
 		if (!search) return message.channel.send(`${client.emotes.error} - Please indicate the title of a song !`);
-		var player = client.memory.get(client.guild, player) || client.memory.set(client.guild, new Player(client));
+		var player = this.client.memory.get(message.guild, player) || this.client.memory.set(message.guild, new Player(client));
 		player.play(message, search, { firstResult: true });
         
         

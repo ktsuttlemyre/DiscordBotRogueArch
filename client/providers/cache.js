3a48bdc6afa0e78e93b420b9f780c611cfdbfda4
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-02-23 05:13:55 -0500

    Create cache.js

---
+++ client/providers/cache.js
@@ -0,0 +1,46 @@
+const { Guild } = require('discord.js');
+const cache = {};
+
+class MemoryCache {
+//	constructor(table) {
+//		super(table, {
+//			idColumn: 'guildID',
+//			dataColumn: 'settings',
+//		});
+//	}
+
+	get(guild, key, defaultValue) {
+		const id = this.constructor.getGuildID(guild);
+    let value = (cache[id])?cache[id][key]:cache[id]={} && undefined;
+    if(value === undefined){
+      return defaultValue;
+    }
+		return value;
+	}
+
+	set(guild, key, value) {
+		const id = this.constructor.getGuildID(guild);
+		return cache[id][key]=value;
+	}
+
+	delete(guild, key) {
+		const id = this.constructor.getGuildID(guild);
+    cache[id][key]=undefined;
+    delete cache[id][key];
+		return undefined;
+	}
+
+	clear(guild) {
+		const id = this.constructor.getGuildID(guild);
+		return cache[id]={};
+	}
+
+	static getGuildID(guild) {
+		if (guild instanceof Guild) return guild.id;
+		if (guild === 'global' || guild === null) return 'global';
+		if (typeof guild === 'string' && /^\d+$/.test(guild)) return guild;
+		throw new TypeError('Invalid guild specified. Must be a Guild instance, guild ID, "global", or null.');
+	}
+}
+
+module.exports = MemoryCache;

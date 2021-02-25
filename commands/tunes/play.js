12812838cf4992cbe98c5513535385c199609270
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-02-25 11:04:43 -0500

    Update play.js

--- commands/tunes/play.js
+++ commands/tunes/play.js
@@ -41,10 +41,70 @@ class PlayCommand extends Command {
 			player = this.client.memory.set(message.guild, 'player', new Player(this.client,options));
 						
 			player.on("trackStart",function(message, track){
-
 				//alert the user of what is now playing
-				GUIMessages.NowPlayingOverloaded(message,track,player);
+				GUIMessages.NowPlayingOverloaded(message,player);
 				//message.channel.send(`Now playing ${track.title} requested by @${track.requestedBy.username} `)
+			})
+			// Send a message when something is added to the queue
+			.on('trackAdd',function(message, queue, track){
+				message.react('🤖');
+				message.react('✔️');
+				GUIMessages.NowPlayingOverloaded(message,player,`${message.member.displayName} has added ${track.title}`);
+			})
+			.on('playlistAdd',function(message, queue, playlist){
+				message.react('🤖');
+				message.react('✔️');
+				GUIMessages.NowPlayingOverloaded(message,player,`${message.member.displayName} has added playlist ${playlist.title}`);
+			})
+			// Send messages to format search results
+			.on('searchResults', (message, query, tracks) => {
+
+			    const embed = new Discord.MessageEmbed()
+			    .setAuthor(`Here are your search results for ${query}!`)
+			    .setDescription(tracks.map((t, i) => `${i}. ${t.title}`))
+			    .setFooter('Send the number of the song you want to play!')
+			    message.channel.send(embed);
+
+			})
+			.on('searchInvalidResponse', (message, query, tracks, content, collector) => {
+
+			    if (content === 'cancel') {
+				collector.stop()
+				return message.channel.send('Search cancelled!')
+			    }
+
+			    message.channel.send(`You must send a valid number between 1 and ${tracks.length}!`)
+
+			})
+			.on('searchCancel', (message, query, tracks) => message.channel.send('You did not provide a valid response... Please send the command again!'))
+			.on('noResults', (message, query) => message.channel.send(`No results found on YouTube for ${query}!`))
+
+			// Send a message when the music is stopped
+			.on('queueEnd', (message, queue) => message.channel.send('Music stopped as there is no more music in the queue!'))
+			.on('channelEmpty', (message, queue) => message.channel.send('Music stopped as there is no more member in the voice channel!'))
+			.on('botDisconnect', (message) => message.channel.send('Music stopped as I have been disconnected from the channel!'))
+
+			// Error handling
+			.on('error', (error, message) => {
+			    switch(error){
+				case 'NotPlaying':
+				    message.channel.send('There is no music being played on this server!')
+				    break;
+				case 'NotConnected':
+				    message.channel.send('You are not connected in any voice channel!')
+				    break;
+				case 'UnableToJoin':
+				    message.channel.send('I am not able to join your voice channel, please check my permissions!')
+				    break;
+				case 'LiveVideo':
+				    message.channel.send('YouTube lives are not supported!')
+				    break;
+				case 'VideoUnavailable':
+				    message.channel.send('This YouTube video is not available!');
+				    break;
+				default:
+				    message.channel.send(`Something went wrong... Error: ${error}`)
+			    }
 			})
 		}
 		/*

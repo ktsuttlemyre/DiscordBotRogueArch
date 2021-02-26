a5e3e39529ec8853ea41074a9c8a2b5245609c6e
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-02-25 20:13:13 -0500

    Create common.js

---
+++ common.js
@@ -0,0 +1,10 @@
+exports.reactions={	      
+	      upvote:'✅',
+	      downvote:'❌',
+	      shipwash:'802028980739768320'
+	     }
+exports.defaultAvatar='https://discord.com/assets/322c936a8c8be1b803cd94861bdfa868.png';
+
+exports.permalinkMessage=function(guild,channel,message){
+  return `https://discord.com/channels/${guild.id}/${channel.id}/{message.id}`;
+}

fff40d88eee0c559e5e5b991e7c35f72f3af09fb
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-02-26 06:50:20 -0500

    Update common.js

--- common.js
+++ common.js
@@ -8,3 +8,16 @@ exports.defaultAvatar='https://discord.com/assets/322c936a8c8be1b803cd94861bdfa8
 exports.permalinkMessage=function(guild,channel,message){
   return `https://discord.com/channels/${guild.id}/${channel.id}/${message.id}`;
 }
+
+exports.filterInPlace=function (a, condition) {
+  let i = 0, j = 0;
+
+  while (i < a.length) {
+    const val = a[i];
+    if (condition(val, i, a)) a[j++] = val;
+    i++;
+  }
+
+  a.length = j;
+  return a;
+}

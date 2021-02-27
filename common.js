daf4e4996e64d135c033a676c8191f9eb6a01fbc
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-02-27 15:03:14 -0500

    Update common.js

--- common.js
+++ common.js
@@ -25,27 +25,25 @@ exports.filterInPlace=function (a, condition) {
 //https://www.fileformat.info/info/unicode/char/search.htm?q=block&preview=entity
 //example
 //progressString('vertical-block',40);
-function(){
-
-	var bars={
-		"vertical-bar":[
-			{"name":"Zero",							"unicode":"",		"glyph":"▕  ▏", "percent":0},
-			{"name":"LOWER ONE EIGHTH BLOCK",		"unicode":"U+2581",	"glyph":"▕▁▏", "percent":12.5},
-			{"name":"LOWER ONE QUARTER BLOCK",		"unicode":"U+2582",	"glyph":"▕▂▏", "percent":25},
-			{"name":"LOWER THREE EIGHTHS BLOCK",	"unicode":"U+2583",	"glyph":"▕▃▏", "percent":37.5},
-			{"name":"LOWER FIVE EIGHTHS BLOCK",		"unicode":"U+2585",	"glyph":"▕▅▏", "percent":62.5},
-			{"name":"LOWER THREE QUARTERS BLOCk",	"unicode":"U+2586",	"glyph":"▕▆▏", "percent":75},
-			{"name":"LOWER SEVEN EIGHTHS BLOCK",	"unicode":"U+2587",	"glyph":"▕▇▏", "percent":87.5},
-			{"name":"FULL BLOCK",					"unicode":"U+2588",	"glyph":" ██", "percent":100},
-		]
-	}
-	exports.progressString=function progressString(type,percent){
-		for(var i=0,l=bars[type].length;i<l;i++){
-			var entry=bars[type][i]
-			if(entry.percent>=percent){
-				return entry.glyph
-			}
+var bars={
+	"vertical-bar":[
+		{"name":"Zero",							"unicode":"",		"glyph":"▕  ▏", "percent":0},
+		{"name":"LOWER ONE EIGHTH BLOCK",		"unicode":"U+2581",	"glyph":"▕▁▏", "percent":12.5},
+		{"name":"LOWER ONE QUARTER BLOCK",		"unicode":"U+2582",	"glyph":"▕▂▏", "percent":25},
+		{"name":"LOWER THREE EIGHTHS BLOCK",	"unicode":"U+2583",	"glyph":"▕▃▏", "percent":37.5},
+		{"name":"LOWER FIVE EIGHTHS BLOCK",		"unicode":"U+2585",	"glyph":"▕▅▏", "percent":62.5},
+		{"name":"LOWER THREE QUARTERS BLOCk",	"unicode":"U+2586",	"glyph":"▕▆▏", "percent":75},
+		{"name":"LOWER SEVEN EIGHTHS BLOCK",	"unicode":"U+2587",	"glyph":"▕▇▏", "percent":87.5},
+		{"name":"FULL BLOCK",					"unicode":"U+2588",	"glyph":" ██", "percent":100},
+	]
+}
+exports.progressString=function progressString(type,percent){
+	for(var i=0,l=bars[type].length;i<l;i++){
+		var entry=bars[type][i]
+		if(entry.percent>=percent){
+			return entry.glyph
 		}
-
 	}
-})();
+
+}
+

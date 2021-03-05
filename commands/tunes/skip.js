610275552e6be4b5ecd244c6661faf3a31820cef
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-05 18:58:58 -0500

    Update skip.js

--- commands/tunes/skip.js
+++ commands/tunes/skip.js
@@ -8,7 +8,7 @@ var _ = require('lodash');
 
 //sound effects https://www.youtube.com/channel/UCok6P4rwxBMun9ghaIV4ufQ
 
-class PlayCommand extends Command {
+class CustomCommand extends Command {
 	constructor() {
 		super('skip', {
 		description: { content: 'skip'},
@@ -63,5 +63,5 @@ class PlayCommand extends Command {
 	}
 }
 
-module.exports = PlayCommand;
+module.exports = CustomCommand;
 

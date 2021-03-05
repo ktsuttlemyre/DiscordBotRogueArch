cec37bd132c0c1a628c3fb5cd1784a11d5f4f720
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-05 18:51:11 -0500

    Create clip.js

---
+++ commands/tunes/clip.js
@@ -0,0 +1,64 @@
+const GUIMessages = require.main.require('./templates/messages');
+const { Command } = require('discord-akairo');
+const { Player } = require("discord-player");
+const emotes={error:":error:"}
+const {reactions,defaultAvatar} = require.main.require('./common');
+const common = require.main.require('./common');
+var _ = require('lodash');
+
+//sound effects https://www.youtube.com/channel/UCok6P4rwxBMun9ghaIV4ufQ
+
+class CustomCommand extends Command {
+	constructor() {
+		super('clip', {
+		description: { content: 'clip'},
+		aliases: ['clip','sound'],
+		category: 'tunes',
+		clientPermissions: ['SEND_MESSAGES'],
+		args: [
+			// {
+			// 	id: 'search',
+			// 	default: '',
+			// 	match: 'content',
+			// },
+			],
+		channelRestriction: 'guild', 
+		});
+	}
+	
+	userPermissions(message) {
+		if (!message.member.roles.cache.some(role => role.name === 'DJ')) {
+			return 'DJ';
+		}
+		return null;
+	}
+
+	async exec(message) {
+		if (!message.member.voice.channel) return message.channel.send(`${emotes.error} - You're not in a voice channel !`);
+		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${emotes.error} - You are not in the same voice channel !`);
+		var player = this.client.memory.get(message.guild, 'player')
+		if(!player){
+			return message.channel.send('No player playing to act on')
+		}
+				
+		const { channel } = message.member.voice;
+
+	    try {
+	      var connection = await channel.join();
+	      const dispatcher = connection
+	        .play(`./sounds/${args[0]}.mp3`)
+	        .on("finish", () => {
+	          channel.leave();
+	        })
+	        .on("error", err => {
+	          channel.leave();
+	          console.error(err);
+	        });
+	    } catch (error) {
+	      console.error(error);
+	    }
+
+	}
+}
+
+module.exports = CustomCommand;

449918f3a8b7a32faa8ad54b296b25225939fd18
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-06 18:43:59 -0500

    Update voiceStateUpdate.js

--- listeners/client/voiceStateUpdate.js
+++ listeners/client/voiceStateUpdate.js
@@ -9,22 +9,55 @@ class ReadyListener extends Listener {
 		});
 	}
 
-	async exec( oldState, newState ) {
+	async exec( oldstate, newstate ) {
     
     
-    
-    //(oldstate.member.id
-    if(old
-      let channel = message.member.voiceChannel;
-      for (let member of channel.members) {
-        member[1].setMute(true)
-      }
-    
-    
-	// Log that the bot is online.
-		this.client.logger.info(`${this.client.user.tag}, ready to serve ${this.client.users.size} users in ${this.client.guilds.size} servers.`, 'ready');
-		// Set the bot status
-		this.client.user.setActivity('Akiro Boilerplate v1.0.0', { type: 'PLAYING' });
+	    var user = newstate.member;
+	    var muteChanged = oldstate.selfMute!=newstate.selfMute
+	    var muted= newstate.selfMute;
+
+	    var roomChanged = oldstate.channelID != newstate.channelID
+	    
+	    var guild = newstate.guild;
+
+	    if(muteChanged){
+		let channel = user.voiceChannel;
+		for (var i=0,l=channel.members.length;i<l;i++) {
+			var member = channel.members[i];
+			if(member.id == user.id){continue}
+			member.setMute(muted)
+		}
+		
+	    }
+		
+	    var channelMap={
+		//general: shiptunes
+		'799879532856475648':'805549728099860480',
+		//social groovy
+		'690661623831986270':'800007831251189821',
+	    }
+		
+	    if(roomChanged){
+		var textChannelID=channelMap[oldstate.channelID];
+		var textChannel=guild.channels.cache.get(textChannelID);
+		if(textChannel){
+			//leave private rooms
+			textChannel.updateOverwrite(user, {
+			    //SEND_MESSAGES: false,
+			    VIEW_CHANNEL: false
+			});
+		}
+		    
+		textChannelID=channelMap[newstate.channelID];
+		textChannel=guild.channels.cache.get(textChannelID);
+		if(textChannel){
+			//join private rooms
+			textChannel.updateOverwrite(user, {
+			    //SEND_MESSAGES: false,
+			    VIEW_CHANNEL: true
+			});
+		}
+	    }
 	}
 }
 

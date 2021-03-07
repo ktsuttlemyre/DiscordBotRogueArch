0dbcda1f9bb59eb24d3ee8647324910f0a76bfcb
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-07 15:23:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -31,36 +31,55 @@ process.on('unhandledRejection', err => {
 
 
   //shutdown gracefully and clean up 
+  //Heroku sends SIGTERM when you restart dynos
   process
   .on('SIGTERM', shutdown('SIGTERM'))
   .on('SIGINT', shutdown('SIGINT'))
   .on('uncaughtException', shutdown('uncaughtException'));
   function shutdown(signal) {
     return (err) => {
-      console.log(`${ signal }...`);
-      if (err){
-        console.error(err.stack || err);
-      }
-      client.guilds.cache.forEach(function(guild){ //iter guilds
-	guild.members.cache.some(function(member){ //iter members
-		if(member.user.bot){ //ignore bots
-			return false;
+	console.log(`Shutting down with signal: ${ signal }\n\t error: `);
+	if (err){
+	console.error(err.stack || err);
+	}
+
+	if(signal == 'SIGTERM'){ //assume restart
+	      return
+	}
+	//other signals assume sleep
+
+	// do cleanup
+	client.guilds.cache.forEach(function(guild){ //iter guilds
+		switch(signal){
+			case 'SIGINT:
+				guild.members.cache.some(function(member){ //iter members
+					if(member.user.bot){ //ignore bots
+						return false;
+					}
+					if(member.voice.serverMute){
+						console.log('unServerMuting', member.displayName);
+						member.voice.setMute(false); //unmute
+					}
+
+					if(member.voice.serverDeaf){
+						console.log('unServerDeafening', member.displayName);
+						member.voice.setDeaf(false); //undefen
+					}
+				}); //end iter members
+			case 'SIGTERM': //heroku sends sigterm for restarting dynos
+			default:
+				var memory=client.memory
+				if(!memory){return}
+				var player=memory.get(guild, 'player');
+				if(!player){return}
+				var queues=memory.get(guild, 'queues')||[];
+				queues.forEach(function(queue){
+					var message = queue.firstMessage
+					if(player.isPlaying(message)){
+					  common.nowPlaying(message,null,'I have crashed or gone to sleep!')
+					}	
+				});
 		}
-		member.voice.setMute(false); //unmute anyone
-		member.voice.setDeaf(false); //undefen anyone
-	}); //end iter members
-        
-	var memory=client.memory
-	if(!memory){return}
-        var player=memory.get(guild, 'player');
-	if(!player){return}
-	var queues=memory.get(guild, 'queues')||[];
-	queues.forEach(function(queue){
-		var message = queue.firstMessage
-		if(player.isPlaying(message)){
-		  common.nowPlaying(message,null,'I have crashed or gone to sleep!')
-		}	
-	})
       }); //end iter guilds
       process.exit(err ? 1 : 0);
 

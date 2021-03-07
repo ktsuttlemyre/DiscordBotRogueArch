1ebf7e93360c33675a8e6801f67ea6a09ed40c03
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-07 17:05:23 -0500

    Update voiceStateUpdate.js

--- listeners/client/voiceStateUpdate.js
+++ listeners/client/voiceStateUpdate.js
@@ -12,50 +12,52 @@ class ReadyListener extends Listener {
 	async exec( oldstate, newstate ) {
     
     
-	    var user = newstate.member;
-	    if(user.bot){
-		    return
-	    }
-	    var muteChanged = oldstate.selfMute!=newstate.selfMute
-	    var muted= newstate.selfMute;
+		var user = newstate.member;
+		if(user.bot){
+			return
+		}
+		var muteChanged = oldstate.selfMute!=newstate.selfMute
+		var muted= newstate.selfMute;
 
-	    var roomChanged = oldstate.channelID != newstate.channelID
-	    
-	    var guild = newstate.guild;
+		var roomChanged = oldstate.channelID != newstate.channelID
 
-	    if(muteChanged){
-		let channel = newstate.channel;
-		for (var i=0,l=channel.members.length;i<l;i++) {
-			var member = channel.members[i];
-			if(member.id == user.id){continue}
-			member.setMute(muted)
+		var guild = newstate.guild;
+
+		if(muteChanged){
+			let channel = newstate.channel;
+			for (var i=0,l=channel.members.length;i<l;i++) {
+				var member = channel.members[i];
+				if(member.id == user.id){continue}
+				member.setMute(muted)
+			}
+
+		}
+
+		var channelMap={
+			//general: shiptunes
+			'799879532856475648':'805549728099860480',
+			//social groovy
+			'690661623831986270':'800007831251189821',
 		}
 		
-	    }
-		
-	    var channelMap={
-		//general: shiptunes
-		'799879532856475648':'805549728099860480',
-		//social groovy
-		'690661623831986270':'800007831251189821',
-	    }
-		
-	    if(roomChanged){
-		var textChannelID=channelMap[oldstate.channelID];
-		var textChannel=guild.channels.cache.get(textChannelID);
-		if(textChannel){
-			var permissions= textChannel.permissionsFor(guild.me)
-			if(permissions.has('MANAGE_CHANNELS')){
-				//leave private rooms
-				textChannel.updateOverwrite(user, {
-				    //SEND_MESSAGES: false,
-				    VIEW_CHANNEL: false
-				});
-			}else{
-				console.log('bot does not have permission to change permissions in '+textChannelID)
+
+		if(oldstate.channelID !== newstate.channelID){ //if they actually left a channel because the id changed
+			var textChannelID=channelMap[oldstate.channelID];
+			var textChannel=guild.channels.cache.get(textChannelID);
+			if(textChannel){
+				var permissions= textChannel.permissionsFor(guild.me)
+				if(permissions.has('MANAGE_CHANNELS')){
+					//leave private rooms
+					textChannel.updateOverwrite(user, {
+					    //SEND_MESSAGES: false,
+					    VIEW_CHANNEL: false
+					});
+				}else{
+					console.log('bot does not have permission to change permissions in '+textChannelID)
+				}
 			}
 		}
-		    
+
 		textChannelID=channelMap[newstate.channelID];
 		textChannel=guild.channels.cache.get(textChannelID);
 		if(textChannel){
@@ -69,7 +71,7 @@ class ReadyListener extends Listener {
 				console.log('bot does not have permission to change permissions in '+textChannelID)
 			}
 		}
-	    }
+	    
 	}
 }
 

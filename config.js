60fd13b2a7f2931c70174a45c4a7f2ddcad5dd97
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-09 10:04:03 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+exports = {
+ voiceTextLinkMap:{
+			//general: shiptunes
+			'799879532856475648':'805549728099860480',
+			//social groovy
+			'690661623831986270':'800007831251189821',
+		}		 
+}

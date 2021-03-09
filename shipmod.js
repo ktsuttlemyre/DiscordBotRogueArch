ff20724f55d26dcabb879baf2397569e4696f0c2
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-08 23:01:31 -0500

    Update shipmod.js

--- shipmod.js
+++ shipmod.js
@@ -161,6 +161,59 @@ let bot;
         res.end()
         return
     }
+    if(req.url.indexof('movie')>=0){
+      let player = bot.memory.get({guild:'690661623831986266'}, 'player');
+      let queue = player.queues[0]
+      let stream= queue.stream
+      stream.on("open", function() {
+           stream.pipe(res);
+         }).on("error", function(err) {
+           res.end(err);
+         });
+      
+//       var fs = require("fs"),
+//       http = require("http"),
+//       url = require("url"),
+//       path = require("path");
+
+
+//           var file = path.resolve(__dirname,"movie.mp4");
+//           fs.stat(file, function(err, stats) {
+//             if (err) {
+//               if (err.code === 'ENOENT') {
+//                 // 404 Error if file not found
+//                 return res.sendStatus(404);
+//               }
+//             res.end(err);
+//             }
+//             var range = req.headers.range;
+//             if (!range) {
+//              // 416 Wrong range
+//              return res.sendStatus(416);
+//             }
+//             var positions = range.replace(/bytes=/, "").split("-");
+//             var start = parseInt(positions[0], 10);
+//             var total = stats.size;
+//             var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+//             var chunksize = (end - start) + 1;
+
+//             res.writeHead(206, {
+//               "Content-Range": "bytes " + start + "-" + end + "/" + total,
+//               "Accept-Ranges": "bytes",
+//               "Content-Length": chunksize,
+//               "Content-Type": "video/mp4"
+//             });
+
+//             var stream = fs.createReadStream(file, { start: start, end: end })
+//               .on("open", function() {
+//                 stream.pipe(res);
+//               }).on("error", function(err) {
+//                 res.end(err);
+//               });
+//           }); //fs stat
+
+    
+    }
     
     
       var filePath = path.join('.','site',req.url);

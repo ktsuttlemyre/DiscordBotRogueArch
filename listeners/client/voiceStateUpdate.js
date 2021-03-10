fc9b0269ebb162b76dd669edd487aa7ff9515751
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-09 20:54:50 -0500

    Update voiceStateUpdate.js

--- listeners/client/voiceStateUpdate.js
+++ listeners/client/voiceStateUpdate.js
@@ -18,58 +18,57 @@ class ReadyListener extends Listener {
 		let guild = newstate.guild;
 		
 		// voice-text-channel-link
-		let roomChanged = oldstate.channelID != newstate.channelID
+		let roomChanged = (oldstate.channelID || newstate.channelID) && oldstate.channelID !== newstate.channelID
 		let channelMap=config.voiceTextLinkMap;
+		
+		let permissionsNeeded = ['VIEW_CHANNEL','MANAGE_CHANNELS'];
 		    
 		//enter new chatroom
-		let textChannelID=channelMap[newstate.channelID];
-		let textChannel=guild.channels.cache.get(textChannelID);
+		let textChannelID = channelMap[newstate.channelID];
+		let textChannel = guild.channels.cache.get(textChannelID);
+		let permissions;
 		if(textChannel){
-			let permissions= textChannel.permissionsFor(guild.me)
-			if(permissions.has(['VIEW_CHANNEL','MANAGE_CHANNELS'],false)){
+			permissions = textChannel.permissionsFor(guild.me)
+			if(permissions.has(permissionsNeeded,false)){
 				textChannel.updateOverwrite(thisMember, {
 				    //SEND_MESSAGES: false,
 				    VIEW_CHANNEL: true
 				});
 			}else{
-				console.log('bot does not have permission to change permissions in '+textChannelID)
+				console.log('bot does not have permission to change permissions in '+textChannel.name)
 			}
 		}
 		
 		//leave old chatroom (if they left a room)
-		if(oldstate.channelID && oldstate.channelID !== newstate.channelID){ //if they actually left a channel because the id changed
-			textChannelID=channelMap[oldstate.channelID];
-			textChannel=guild.channels.cache.get(textChannelID);
-			if(textChannel){
-				let permissions= textChannel.permissionsFor(guild.me)
-				//console.log(permissions.toArray())
-				if(permissions.has(['VIEW_CHANNEL','MANAGE_CHANNELS'],false)){
-					//leave private rooms
-					textChannel.updateOverwrite(thisMember, {
-					    //SEND_MESSAGES: false,
-					    VIEW_CHANNEL: false
-					});
-				}else{
-					console.log('bot does not have permission to change permissions in '+textChannelID)
-				}
+		textChannelID = channelMap[oldstate.channelID];
+		textChannel = guild.channels.cache.get(textChannelID);
+		if(roomChanged && textChannel){ //if they actually left a channel because the id changed			
+			permissions = textChannel.permissionsFor(guild.me)
+			//console.log(permissions.toArray())
+			if(permissions.has(permissionsNeeded,false)){
+				//leave private rooms
+				textChannel.updateOverwrite(thisMember, {
+				    //SEND_MESSAGES: false,
+				    VIEW_CHANNEL: false
+				});
+			}else{
+				console.log('bot does not have permission to change permissions in '+textChannel.name)
 			}
 		}
 		
 		
-		if(newstate.channel){
+		//handle amongus mute mode
+		let muteChanged = oldstate.selfMute!=newstate.selfMute
+		let muted= newstate.selfMute;
+		if(newstate.channel && muteChanged){
 			let amongusMode = this.client.memory.channelGet(newstate, 'amongusMode');
 			//mute handler
 			if(amongusMode){
-				let muteChanged = oldstate.selfMute!=newstate.selfMute
-				let muted= newstate.selfMute;
-				if(muteChanged){
-					let channel = newstate.channel;
-					channel.members.forEach(function(member){
-						if(member.id == thisMember.id){return}
-						member.voice.setMute(muted);
-					}); //end members
+				newstate.channel.members.forEach(function(member){
+					if(member.id == thisMember.id){return}
+					member.voice.setMute(muted);
+				}); //end members
 
-				}
 			}
 		}
 	    

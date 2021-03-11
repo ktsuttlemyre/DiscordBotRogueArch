7d19fa8f3198b1cfdd0cc241641a18e9771c5b7d
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-10 23:21:13 -0500

    Update fetchall.js

--- commands/general/fetchall.js
+++ commands/general/fetchall.js
@@ -34,62 +34,9 @@ class PlayCommand extends Command {
 	}
 
 	async exec(message) {
-    /*async function lots_of_messages_getter(channel, limit = 500) {
-        const sum_messages = [];
-        let last_id;
 
-        while (true) {
-            const options = { limit: 100 };
-            if (last_id) {
-                options.before = last_id;
-            }
-
-            const messages = await channel.fetchMessages(options);
-            sum_messages.push(...messages.array());
-            last_id = messages.last().id;
-
-            if (messages.size != 100 || sum_messages >= limit) {
-                break;
-            }
-        }
-
-        return sum_messages;
-    }
-    lots_of_messages_getter(message.channel,Infinity);
-    */
-    
-	async function getMessages(channel, limit) {
-		if(!limit){
-			limit=Infinity
-		}
-		let out= []
-		// if (limit <= 100) {
-		//   let messages = await channel.messages.fetch({ limit: limit })
-		//   out.push.apply(out,messages.array())
-		// } else {
-		let rounds = (limit / 100) + (limit % 100 ? 1 : 0)
-		let last_id = ""
-		for (let x = 0; x < rounds; x++) {
-			const options = {
-				limit: 100
-			}
-			if (last_id.length > 0) {
-				options.before = last_id
-			}
-			const messages = await channel.messages.fetch(options);
-			const messageArray = messages.array();
-			if(!messageArray.length){
-				break
-			}
-			out.push.apply(out,messageArray)
-			//console.log('messages.length',messageArray.length)
-			last_id = messageArray[(messageArray.length - 1)].id
-		}
-		//}
-		return out
-	}
-    let messages = await getMessages(message.channel);
-    message.channel.send(`got ${messages.length} messages`);
+	    let messages = await common.getMessages(message.channel);
+	    message.channel.send(`got ${messages.length} messages`);
     
     
 	}

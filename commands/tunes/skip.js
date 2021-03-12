9cd8f659e65036e6b37388e1f19c390206af957a
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-11 20:36:51 -0500

    Update skip.js

--- commands/tunes/skip.js
+++ commands/tunes/skip.js
@@ -13,7 +13,7 @@ class CustomCommand extends Command {
 		super(path.parse(__filename).name, {
 		description: { content: 'skip'},
 		aliases: ['skip','next'],
-		category: path.basename(path.dirname(filename)),
+		category: path.basename(path.dirname(_filename)),
 		clientPermissions: ['SEND_MESSAGES'],
 		args: [
 			// {

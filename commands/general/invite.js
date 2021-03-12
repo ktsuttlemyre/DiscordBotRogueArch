aff8ea1c2c5ab1561d0cf8f442ad50bb407539e1
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-11 20:31:57 -0500

    Update invite.js

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -13,7 +13,7 @@ class CustomCommand extends Command {
 		super(path.parse(__filename).name, {
 		description: { content: 'invite'},
 		aliases: ['invite'],
-		category: path.basename(path.dirname(filename)),
+		category: path.basename(path.dirname(__filename)),
 		clientPermissions: ['SEND_MESSAGES'],
 		args: [
 			// {

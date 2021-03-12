e0a0c435923d0a55fc28ea7b8140119beb647095
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-11 20:26:52 -0500

    Update amongus.js

--- commands/general/amongus.js
+++ commands/general/amongus.js
@@ -4,16 +4,16 @@ const { Player } = require("discord-player");
 const emotes={error:":error:"}
 const {reactions,defaultAvatar} = require.main.require('./common');
 const common = require.main.require('./common');
-var _ = require('lodash');
+const _ = require('lodash');
+const path = require('path');
 
-//sound effects https://www.youtube.com/channel/UCok6P4rwxBMun9ghaIV4ufQ
 
-class PlayCommand extends Command {
+class CustomCommand extends Command {
 	constructor() {
-		super('amongus', {
+		super(path.parse(__filename).name, {
 		description: { content: 'sets amongus muting'},
 		aliases: [''],
-		category: 'general',
+		category: path.basename(path.dirname(filename)),
 		clientPermissions: ['SEND_MESSAGES'],
 		args: [
 			 {

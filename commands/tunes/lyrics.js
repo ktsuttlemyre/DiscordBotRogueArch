12ab3b40a3a5af0458c6ca3a5d5bbc69a06e8d7d
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-11 20:35:38 -0500

    Update lyrics.js

--- commands/tunes/lyrics.js
+++ commands/tunes/lyrics.js
@@ -15,7 +15,7 @@ class CustomCommand extends Command {
 		super(path.parse(__filename).name, {
 		description: { content: 'lyrics'},
 		aliases: ['lyrics','ly'],
-		category: path.basename(path.dirname(filename)),
+		category: path.basename(path.dirname(__filename)),
 		clientPermissions: ['SEND_MESSAGES'],
 		args: [
 			// {

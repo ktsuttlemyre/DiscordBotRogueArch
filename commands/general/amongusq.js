665198e04bac384d37a2e6255c94396d2a7c11d0
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-13 19:41:38 -0500

    Update amongusq.js

--- commands/general/amongusq.js
+++ commands/general/amongusq.js
@@ -30,13 +30,19 @@ class CustomCommand extends Command {
 	    let amongEmojis=['<:AmongUsDeadOrange:800120891857829919>','<:AmongButton:800807792193306684>','<:among_us_report:800804847728853022>','<:amongusshhhhh:800119749056921640>']
 	    let varName = 'amongusq'
 	    let q = this.client.memory.get(message, varName) || [];
+	    let lastMessage = this.client.memory.get(message, 'lastAmongusQ');
 	    let person = message.member||message.author
 	    let name = person.displayName || person.tag
 	    if(q.indexOf(name)<0){
 	    	q.push(name);
 	    }
-	    message.delete()
-	    message.channel.send({embed:{
+	    if(lastMessage){
+		    lastMessage.delete()
+	    }
+	    message.delete();
+	    this.client.memory.set(message, varName, q);
+	    
+	    await lastMessage = message.channel.send({embed:{
 		    			title:'<:amongus:800119041452146731> AmongUs Queue '+_.sample(amongEmojis),
 					description:q.join('\n'),
 					footer: {
@@ -44,7 +50,7 @@ class CustomCommand extends Command {
 						//icon_url: 'https://i.imgur.com/wSTFkRM.png',
 					},
 	    			}});
-	    this.client.memory.set(message, varName, q);
+	    this.client.memory.set(message, 'lastAmongusQ', lastMessage);
 	}
 }
 

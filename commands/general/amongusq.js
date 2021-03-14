aca76d4bf44e6071969fb64772abaefd3334ea3c
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-13 19:17:00 -0500

    Create amongusq.js

---
+++ commands/general/amongusq.js
@@ -0,0 +1,43 @@
+const GUIMessages = require.main.require('./templates/messages');
+const { Command } = require('discord-akairo');
+const { Player } = require("discord-player");
+const emotes={error:":error:"}
+const {reactions,defaultAvatar} = require.main.require('./common');
+const common = require.main.require('./common');
+const _ = require('lodash');
+const path = require('path');
+
+
+class CustomCommand extends Command {
+	constructor() {
+		super(path.parse(__filename).name, {
+		description: { content: 'sets amongus muting'},
+		aliases: ['amongusqueue'],
+		category: path.basename(path.dirname(__filename)),
+		clientPermissions: ['SEND_MESSAGES'],
+//		args: [
+// 			 {
+// 			 	id: 'user',
+// 			 	default: '',
+// 			 	match: 'content',
+// 			 },
+// 			],
+		channelRestriction: 'guild', 
+		});
+	}
+
+	async exec(message) {
+    let varName = 'amongusq'
+    let q = this.client.memory.get(message, varName) || [];
+    let person = message.member||message.author
+    let name = person.displayName || person.tag
+    q.push(name)
+    message.delete()
+    message.send({embed:{title:'AmongUs Queue',description:q.join('\n')}});
+    
+		this.client.memory.set(message, varName, q);
+	}
+}
+
+module.exports = CustomCommand;
+

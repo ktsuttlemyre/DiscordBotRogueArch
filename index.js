4d7b76c6aa09e884e5073665c5e53898ad7cde63
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-15 02:54:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
+const http = require('http')
+const fs = require('fs')
 const path = require('path');
 const BoilerplateClient = require(path.join(__dirname,'/client/BoilerplateClient'));
 require('dotenv').config();
-const client = new BoilerplateClient({ owner: process.env.OWNERS, token: process.env.DISCORD_TOKEN });
+const shiptunes = new BoilerplateClient({ owner: process.env.OWNERS, token: process.env.DISCORD_TOKEN });
+const shipmod = new BoilerplateClient({ owner: process.env.OWNERS, token: process.env.SHIPMOD_TOKEN });
 const Sentry = require('@sentry/node');
 
-// Load Logger
-if (process.env.SENTRY_URL) {
-	try {
-		client.logger.log('Sentry Monitoring Loading...');
-		Sentry.init({ dsn: process.env.SENTRY_URL, environment: process.env.NODE_ENV });
-		client.logger.info('Sentry Monitoring Loaded and Ready!');
-	}
-	catch (e) {
-		client.logger.error(e);
-	}
-}
+const bots=[shiptunes,shipmod];
 
-client.on('disconnect', () => client.logger.warn('Connection lost...'))
-	.on('reconnect', () => client.logger.info('Attempting to reconnect...'))
-	.on('error', err => client.logger.error(err))
-	.on('warn', info => client.logger.warn(info));
-client.start();
+function init(client){
+	// Load Logger
+	if (process.env.SENTRY_URL) {
+		try {
+			client.logger.log('Sentry Monitoring Loading...');
+			Sentry.init({ dsn: process.env.SENTRY_URL, environment: process.env.NODE_ENV });
+			client.logger.info('Sentry Monitoring Loaded and Ready!');
+		}
+		catch (e) {
+			client.logger.error(e);
+		}
+	}
+
+	client.on('disconnect', () => client.logger.warn('Connection lost...'))
+		.on('reconnect', () => client.logger.info('Attempting to reconnect...'))
+		.on('error', err => client.logger.error(err))
+		.on('warn', info => client.logger.warn(info));
+	client.start();
+}
+bots.forEach(init);
 
 process.on('unhandledRejection', err => {
-	client.logger.error('An unhandled promise rejection occured');
-	client.logger.stacktrace(err);
+	bots.forEach(function(client){
+		client.logger.error('An unhandled promise rejection occured');
+		client.logger.stacktrace(err);
+	}
 });
 
 
@@ -85,3 +95,91 @@ process.on('unhandledRejection', err => {
     };
   }//end graceful shutdown
 
+	
+	
+(function server(){
+  const requestListener = function (req, res) {
+
+    if(req.url=='/heartbeat'){
+         res.writeHead(200);
+         res.end('Hello, World!');
+         return
+    }
+    
+    if(req.url=='/'){
+        let file = fs.readFileSync(path.join('.','site','index.html'),'utf-8')
+        res.write(file)
+        res.end()
+        return
+    }
+    if(req.url.indexof('movie')>=0){
+      let player = bot.memory.get({guild:'690661623831986266'}, 'player');
+      let queue = player.queues[0]
+      let stream= queue.stream
+      stream.on("open", function() {
+           stream.pipe(res);
+         }).on("error", function(err) {
+           res.end(err);
+         });
+      
+//       var fs = require("fs"),
+//       http = require("http"),
+//       url = require("url"),
+//       path = require("path");
+
+
+//           var file = path.resolve(__dirname,"movie.mp4");
+//           fs.stat(file, function(err, stats) {
+//             if (err) {
+//               if (err.code === 'ENOENT') {
+//                 // 404 Error if file not found
+//                 return res.sendStatus(404);
+//               }
+//             res.end(err);
+//             }
+//             var range = req.headers.range;
+//             if (!range) {
+//              // 416 Wrong range
+//              return res.sendStatus(416);
+//             }
+//             var positions = range.replace(/bytes=/, "").split("-");
+//             var start = parseInt(positions[0], 10);
+//             var total = stats.size;
+//             var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+//             var chunksize = (end - start) + 1;
+
+//             res.writeHead(206, {
+//               "Content-Range": "bytes " + start + "-" + end + "/" + total,
+//               "Accept-Ranges": "bytes",
+//               "Content-Length": chunksize,
+//               "Content-Type": "video/mp4"
+//             });
+
+//             var stream = fs.createReadStream(file, { start: start, end: end })
+//               .on("open", function() {
+//                 stream.pipe(res);
+//               }).on("error", function(err) {
+//                 res.end(err);
+//               });
+//           }); //fs stat
+
+    
+    }
+    
+    
+      var filePath = path.join('.','site',req.url);
+//       if(filePath.endsWith('/')){
+//         filePath.path.join(filePath,'index.html');
+//       }
+      // Browser will autorequest 'localhost:8000/favicon.ico'
+      if ( !(req.url.endsWith("favicon.ico")) ) {
+        let file = fs.readFileSync(filePath,'utf-8')
+        res.write(file)
+      }
+      res.end();
+    
+  }
+
+  const server = http.createServer(requestListener);
+  server.listen(process.env.PORT||80);
+})();

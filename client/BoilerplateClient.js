5a4e0449831bc305c43bd817075e4052473a2d03
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-15 03:49:11 -0400

    Update BoilerplateClient.js

--- client/BoilerplateClient.js
+++ client/BoilerplateClient.js
@@ -29,7 +29,7 @@ class BoilerplateClient extends AkairoClient {
 		this.logger = Logger;
 		// Init Command Handler
 		this.commandHandler = new CommandHandler(this, {
-			directory: './commands/tunes',
+			directory: this.config.commandDir,
 			aliasReplacement: /-/g,
 			prefix: message => this.settings.get(message.guild, 'prefix', '!'),
 			allowMention: true,

7b6bd830f516e36881c86780a2186163114ceffd
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-16 00:16:47 -0400

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,8 +1,14 @@
 module.exports = {
- voiceTextLinkMap:{
-			//general: shiptunes
-			'799879532856475648':'805549728099860480',
-			//social groovy
-			//'690661623831986270':'800007831251189821',
-		}		 
+	voiceTextLinkMap:{
+		//general: shiptunes
+		'799879532856475648':'805549728099860480',
+		//social groovy
+		//'690661623831986270':'800007831251189821',
+	},
+	eventRoomMap:{
+		'789704651414306817':'AMONGUS',
+		'799429356605800458':'MOVIE',
+		'801473602611445790':'SOCIAL',
+		'':'SPEEDRUN',
+	}
 }

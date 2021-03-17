7500d4893b7084dd554cb970b02fecb5176595c6
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-16 21:39:18 -0400

    Update poll.js

--- bots/shipmod/commands/general/poll.js
+++ bots/shipmod/commands/general/poll.js
@@ -15,7 +15,7 @@ class CustomCommand extends Command {
 		clientPermissions: ['SEND_MESSAGES','MANAGE_MESSAGES'],
 		args: [
 			 {
-			 	id: 'queue',
+			 	id: 'anon',
 			 	default: '',
 			 	match: 'content',
 			 },
@@ -24,77 +24,58 @@ class CustomCommand extends Command {
 		});
 	}
 
-	async exec(message, { queue }) {
-		console.log('roomMap',roomMap)
-		if(!queue){
-			queue=roomMap[message.channel.id] || message.channel.name;
-		}
-		queue=queue.trim().toUpperCase();
+	async exec(message, { anon }) {
+		let id = message.id;
+		let user = message.member||message.author
 		
 		let randomEmoji = [];
-		let title = `${queue} Event Queue `
-		let varName = queue+'_queue';
-		let q = this.client.memory.get(message, varName) || new Collection();
-		let lastMessageID = varName+'LastMessage'
-		let lastMessage = this.client.memory.get(message, lastMessageID);
-		let user = message.member||message.author
-		switch(queue){
-			case 'AMONGUS':
-				randomEmoji = ['<:AmongUsDeadOrange:800120891857829919>',
-					       '<:AmongButton:800807792193306684>',
-					       '<:among_us_report:800804847728853022>',
-					       '<:amongusshhhhh:800119749056921640>'
-					      ]
-				title = '<:amongus:800119041452146731> AmongUs Event Queue '+_.sample(randomEmoji)
-				break;
-			default:
-				
-				//message.channel.send('No queue associated with '+queue);
-
+		let varName = user.id+'_poll';
+		let pollObj = this.client.memory.getChannel(message, varName) || {};
+		
+		
+		//new Collection()
+		
+// 		if(!q.get(user)){
+// 			q.set(user.id, user);
+// 		}
+		if(pollObj.lastMessage){
+			pollObj.lastMessage.delete();
 		}
-		if(!q.get(user)){
-			q.set(user.id, user);
-		}
-		if(lastMessage){
-			lastMessage.delete()
-		}
-		message.delete();
-		this.client.memory.set(message, varName, q);
+		message.delete && message.delete();
+		this.client.memory.set(message, varName, pollObj);
 		
 		//Render
-		let qDisplay = []
-		q.each(function(user){
-			//let inGame = (message.author||message.member).voice.channel.members.
-			let name = user.displayName || user.tag;
-			let voiceChannel = user.voice.channel;
-			let notAFK=null;
-			let inGuildChannel=null;
-			if(voiceChannel){
-				notAFK = message.guild.afkChannelID != voiceChannel.id
-				inGuildChannel = voiceChannel.guild.id == message.guild.id
+// 		let qDisplay = []
+// 		q.each(function(user){
+// 			//let inGame = (message.author||message.member).voice.channel.members.
+// 			let name = user.displayName || user.tag;
+// 			let voiceChannel = user.voice.channel;
+// 			let notAFK=null;
+// 			let inGuildChannel=null;
+// 			if(voiceChannel){
+// 				notAFK = message.guild.afkChannelID != voiceChannel.id
+// 				inGuildChannel = voiceChannel.guild.id == message.guild.id
+// 			}
+// 			qDisplay.push("\t "+common.reactions[((voiceChannel && notAFK && inGuildChannel)?'green':(user.presence.status === 'online')?'yellow':'red')+'-circle']+` [${name}]( https://discordapp.com/users/${user.id})` )
+// 		})
+		
+		let embed = {
+			title:title,
+			description:qDisplay.join('\n'),
+			//footer: {
+			//	text: `type \`!rsvp ${queue.toLowerCase()}\` to be added to the queue!`,
+			//	//icon_url: 'https://i.imgur.com/wSTFkRM.png',
+			//},
+			fields: [
+					{
+						name: '\u200b',
+						value: `React to this message to vote. Poll is ${(anon)} `,
+						inline: false,
+					},
+				]
 			}
-			qDisplay.push("\t "+common.reactions[((voiceChannel && notAFK && inGuildChannel)?'green':(user.presence.status === 'online')?'yellow':'red')+'-circle']+` [${name}]( https://discordapp.com/users/${user.id})` )
-		})
-		
-		
-		let suffix = ((roomMap[message.channel.id]||'').toLowerCase()==queue.toLowerCase())? '' : queue.toLowerCase();
 		//Send
-		lastMessage = await message.channel.send({embed:{
-					title:title,
-					description:qDisplay.join('\n'),
-					//footer: {
-					//	text: `type \`!rsvp ${queue.toLowerCase()}\` to be added to the queue!`,
-					//	//icon_url: 'https://i.imgur.com/wSTFkRM.png',
-					//},
-					fields: [
-							{
-								name: '\u200b',
-								value: `type \`!rsvp ${suffix}\` to be added to the queue!`,
-								inline: false,
-							},
-						]
-					}
-				});
+		lastMessage = await message.channel.send({embed:embed});
 		//save old message to delete leater
 		this.client.memory.set(message, lastMessageID, lastMessage);
 	}

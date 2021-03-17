12c24db41e1caff08d1b56d4f1da3937cd3f2b10
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-16 20:00:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const Sentry = require('@sentry/node');
 const i18n = require("i18n");
 
+const bots = {};
 
 const { readdirSync } = require('fs')
 const getDirectories = source =>

91e57915cc712cb416a4d30aba9183df5a345475
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-17 16:33:06 -0400

    Update voiceStateUpdate.js

--- bots/shipmod/listeners/client/voiceStateUpdate.js
+++ bots/shipmod/listeners/client/voiceStateUpdate.js
@@ -4,10 +4,10 @@ const commandVars = require.main.require('./common').commandVars(__filename);
 
 class CustomListener extends Listener {
 	constructor() {
-		super(commandVars.name, {
-			emitter: commandVars.category,
-			event: commandVars.name,
-			category: commandVars.category,
+		super('voiceStateUpdate', {
+			emitter: 'client',
+			event: 'voiceStateUpdate',
+			category: 'client',
 		});
 	}
 

cf7af08bc89d02418b60caffa8f8e778266192d1
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-17 16:32:27 -0400

    Update messageReactionRemove.js

--- bots/shipmod/listeners/client/messageReactionRemove.js
+++ bots/shipmod/listeners/client/messageReactionRemove.js
@@ -9,10 +9,10 @@ const commandVars = require.main.require('./common').commandVars(__filename);
 
 class CustomListener extends Listener {
 	constructor() {
-		super(commandVars.name, {
-			emitter: commandVars.category,
-			event: commandVars.name,
-			category: commandVars.category,
+		super('messageReactionRemove', {
+			emitter: 'client',
+			event: 'messageReactionRemove',
+			category: 'client',
 		});
 	}
 

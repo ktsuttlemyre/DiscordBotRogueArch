4f57ba0d698260ed4e760de74338cc239060b37e
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-17 16:19:59 -0400

    Update messageReactionRemove.js

--- bots/shipmod/listeners/client/messageReactionRemove.js
+++ bots/shipmod/listeners/client/messageReactionRemove.js
@@ -7,7 +7,7 @@ const { Listener } = require('discord-akairo');
 const config = require.main.require('./config');
 const commandVars = require.main.require('./common').commandVars(__filename);
 
-class ReadyListener extends Listener {
+class CustomListener extends Listener {
 	constructor() {
 		super(commandVars.name, {
 			emitter: commandVars.category,
@@ -28,4 +28,4 @@ class ReadyListener extends Listener {
 	}
 }
 
-module.exports = ReadyListener;
+module.exports = CustomListener;

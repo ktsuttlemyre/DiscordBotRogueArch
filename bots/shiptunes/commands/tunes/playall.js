54bcf034c7c623d936126e14bff54d9f87ef2499
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-18 21:33:32 -0400

    Update playall.js

--- bots/shiptunes/commands/tunes/playall.js
+++ bots/shiptunes/commands/tunes/playall.js
@@ -1,26 +1,26 @@
 const GUIMessages = require.main.require('./templates/messages');
 const { Command } = require('discord-akairo');
-const { Player } = require("discord-player");
-const emotes={error:":error:"}
+const emotes={error:":warning:"}
 const {reactions,defaultAvatar} = require.main.require('./common');
 const common = require.main.require('./common');
 const commandVars = common.commandVars(__filename);
 const _ = require('lodash');
-
+const path = require('path');
+const util = require.main.require('./util');
 
 class CustomCommand extends Command {
 	constructor() {
 		super(commandVars.name, {
-		description: { content: 'pause the music'},
-		//aliases: [commandVars.name],
+		description: { content: 'plays all of youtube results [name/URL]'},
+		aliases: [commandVars.name],
 		category: commandVars.category,
 		clientPermissions: ['SEND_MESSAGES'],
 		args: [
-			// {
-			// 	id: 'search',
-			// 	default: '',
-			// 	match: 'content',
-			// },
+			{
+				id: 'search',
+				default: '',
+				match: 'content',
+			},
 			],
 		channelRestriction: 'guild', 
 		});
@@ -32,24 +32,69 @@ class CustomCommand extends Command {
 		}
 		return null;
 	}
-  
+	
 	requirements(message,player){
 		let blocked = '';
 		if (!message.member.voice.channel) blocked = `${emotes.error} - You're not in a voice channel !`;
 		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) blocked = `${emotes.error} - You are not in the same voice channel !`;
-		if(!player){blocked = 'No player currently playing';}
-		return blocked
-	}
-
-	async exec(message) {
-		let player = this.client.memory.get(message, 'player')
-		let blocked = requirements(message,player);
+		//if(!player){blocked = 'No player currently playing';}
 		if(blocked){
-			return this.handler.emit('commandBlocked',message,this,blocked);
+			this.handler.emit('commandBlocked',message,this,blocked);
 		}
-		var command = this.client.commandHandler.findCommand('play');
-		await GUIMessages.nowPlaying(message,player,'paused');
+		return blocked;
+	}
+	
+	async exec(message, { search }) {
+		if(this.requirements(message)){
+			return;
+		}
+		var player = this.client.memory.channelGet(message, 'player') || this.client.memory.channelSet(message, 'player', util.player.create(message,this.client));
+		
+		var queue=player.getQueue(message);
+		if(queue){
+			if(queue.paused || queue.stopped){
+				if(player.resume(message)){
+					await GUIMessages.nowPlaying(message,player,"Continuing where we left off "+common.randomMusicEmoji());
+					return
+				}else{
+					let track = player.nowPlaying(message);
+					if(track){
+						await player.play(message,player.nowPlaying(message));
+					}else{
+						//do background
+						await util.player.playBackgroundPlaylist(message,player);
+					}
+				}
+				return //it was paused or stopped so we should have fixed it by now
+			}
+		}
+		if(player.isPlaying(message)){
+			if(!search && !message.attachments){
+				return message.channel.send(`${emotes.error} - Please indicate the title of a song!`);
+			}
+		}else{
+			if(!search && !message.attachments){
+				await util.player.play
+				Playlist(message,player);
+				return
+			}
+		}
+		
+			
+
+		if(message.attachments){
+			await player.play(message, search, { isAttachment:true });
+		}else{
+			await player.play(message, search, { firstResult: false });
+		}
+		//The player was originallly in background mode
+		//so advance past the backgroud music and start playing the users' requests
+		if(player.backgroundPlaylist){
+			player.backgroundPlaylist=false;
+			await player.skip(message);
+		}
 	}
 }
 
 module.exports = CustomCommand;
+

e81e18716c0a18793a113a142c1aea01a242f7fb
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-19 14:32:23 -0400

    Update messages.js

--- util/messages.js
+++ util/messages.js
@@ -9,42 +9,32 @@ const _ = require('lodash');
 const web = require.main.require('./web');
 const yaml = require('js-yaml');
 
-module.exports.encapsulate = function(message,input){
-	if(!input){
-		input=message.content;
-	}
-  	  let doc=input;
-	  if(typeof doc == 'string' && doc.indexOf('---\n')==0){
-	    // Get document, or throw exception on error
-	    try { //TODO test yaml
-	      doc = yaml.load(input);
-	    } catch (e) {
-	      //console.error(e);
-	      //message.channel.send(e.toString())
-	      doc=input;
-	    }
-	  }
-	    var type = typeof doc;
-	    if(type == 'string'){
-		    let split = input.split('\n');
-		    doc = {};
-		    if(split.length==1){
-		    	doc.title = '\t '+split[0];
-		    }else{
-			doc.title = split.shift();
-			doc.description = split.join('\n')
-		    }
-	    }else if(Array.isArray(doc)){
-		    return message.channel.send('Can not process arrays');
-	    }
-	    let user = message.member || message.author
-	    let author = {
-	      name: user.displayName || user.tag,
-	      icon_url: message.author.avatarURL() || common.defaultAvatar,
-	      url: ` https://discordapp.com/users/${user.id}`,
-	    }
-	    doc.author=author;
-	    message.channel.send({embed:doc});
-	    !message.deleted && message.delete();
+module.exports.encapsulate = function(message,override){
+	if(!override){
+		override=message.content;
 	}
 
+	let doc = override || {}
+	var type = typeof doc;
+	if(type == 'string'){
+		let split = input.split('\n');
+		doc = {};
+		if(split.length==1){
+			doc.title = '\t '+split[0];
+		}else{
+		doc.title = split.shift();
+		doc.description = split.join('\n')
+		}
+	}else if(Array.isArray(doc)){
+		return message.channel.send('Can not process arrays');
+	}
+	let user = message.member || message.author
+	let author = {
+		name: user.displayName || user.tag,
+		icon_url: message.author.avatarURL() || common.defaultAvatar,
+		url: ` https://discordapp.com/users/${user.id}`,
+	}
+	doc.author=author;
+	message.channel.send({embed:doc});
+	!message.deleted && message.delete();
+}

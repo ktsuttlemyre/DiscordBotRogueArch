45d24198f286a0b2fb76aae3977cb101cfd5824c
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-18 20:19:31 -0400

    Update playlists.js

--- util/playlists.js
+++ util/playlists.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 
 //https://www.reddit.com/dev/api#GET_new
-module.exports.subreddit = function(subreddit,sort,before,callback){
+module.exports.subredditArray = function(subreddit,sort,before,callback){
   if(typeof before == 'function'){
     callback=before;
     before='';
@@ -12,5 +12,27 @@ module.exports.subreddit = function(subreddit,sort,before,callback){
   }
   return fetch(`https://api.reddit.com/r/${subreddit}/${sort}.json?limit=100`); //?sort=top&t=day&limit=1`)
     .then(response => response.json())
-    .then(response => callback);
+    .then(response => function(json){
+      let youtubeLinks=[]  
+      let text = JSON.stringify(json,null,2).replace(/(\r\n|\n|\r)/gm,"\n");
+      if(text){
+        text=text.split(/\s+/);
+        //console.log("text",text)
+        text.forEach(function(word){
+          if(word.length<11){
+            return
+          }
+          let youtube = web.getYoutubeHash(word);
+          if(youtube){
+            youtubeLinks.push(youtube);
+          }
+        }); //forEach
+      }
+      return youtubeLinks;
+    });
 }
+
+
+
+
+

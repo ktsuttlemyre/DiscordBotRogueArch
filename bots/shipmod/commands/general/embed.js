af7b365760f876f9ff7db9941d644f6c113ef96b
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-19 14:31:48 -0400

    Update embed.js

--- bots/shipmod/commands/general/embed.js
+++ bots/shipmod/commands/general/embed.js
@@ -3,6 +3,7 @@ const common = require.main.require('./common');
 const { Command } = require('discord-akairo');
 const util = require.main.require('./util');
 const commandVars = common.commandVars(__filename);
+const yaml = require('js-yaml');
 
 class CustomCommand extends Command {
 	constructor() {
@@ -30,7 +31,20 @@ class CustomCommand extends Command {
 // 	}
 
 	async exec(message,{ input }) {
-		util.messages.encapsulate(message,input);
+		let doc=input;
+		if(typeof doc == 'string'){
+			doc=doc.trim();
+			if( doc.startsWith('---\n') || ( doc.startsWith('{') && doc.endsWith('}') || doc.startsWith('[') && doc.endsWith(']') )  ){
+				// Get document, or throw exception on error
+				try { //TODO test yaml
+					doc = yaml.load(input);
+				} catch (e) {
+					console.error(e);
+					doc=input;
+				}
+			}
+		}
+		util.messages.encapsulate(message,doc);
 	}
 }
 

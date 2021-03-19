c7ccff4818bf74a0b128607cacfd8a7adb7c1ea1
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-18 20:04:35 -0400

    Update playlists.js

--- util/playlists.js
+++ util/playlists.js
@@ -11,7 +11,7 @@ module.exports.subreddit = function(subreddit,sort,before,callback){
   if(Array.isArray(subreddit)){
     subreddit=subreddit.join('+');
   }
-  return fetch(`https://api.reddit.com/r/${subreddit}/${sort}.json`); //?sort=top&t=day&limit=1`)
+  return fetch(`https://api.reddit.com/r/${subreddit}/${sort}.json?limit=100`); //?sort=top&t=day&limit=1`)
     .then(response => response.json())
     .then(response => callback);
 }

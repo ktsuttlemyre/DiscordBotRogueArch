309d22cf6e7dc58f3ba70e5c367c714a875d8f73
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-18 20:01:20 -0400

    Create playlists.js

---
+++ util/playlists.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch");
+
+//https://www.reddit.com/dev/api#GET_new
+let 
+module.exports.subreddit = function(subreddit,sort,before,callback){
+  if(typeof before == 'function'){
+    callback=before;
+    before='';
+  } 
+  sort=(sort||'new').toLowerCase();
+  if(Array.isArray(subreddit)){
+    subreddit=subreddit.join('+');
+  }
+  return fetch(`https://api.reddit.com/r/${subreddit}/${sort}.json`); //?sort=top&t=day&limit=1`)
+    .then(response => response.json())
+    .then(response => callback);
+}

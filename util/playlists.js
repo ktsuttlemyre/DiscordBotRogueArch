a845f29f5650b1a904399024741762cfda5b499c
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-20 16:47:31 -0400

    Update playlists.js

--- util/playlists.js
+++ util/playlists.js
@@ -34,5 +34,78 @@ module.exports.subredditArray = function(subreddit,sort,before,callback){
 
 
 
+module.exports.fetchSubreddit = async function fetchMessages(subreddit, options, callback) {
+	    if(typeof options == 'function'){
+	    	callback=options
+	    	options={}
+	    }
+		let opts={limit:20};
+
+		let loadedAllMessages = false; //denote all messages are loaded
+		let breakOut = false;  //stop loading any more messages
+
+		let gIndex=0;
+		let gOffset=0;
+		let nBuffer=5;
+
+		const array = [];
+
+		let _processTick=async function(resolve){
+			console.log('processing tick');
+			if(breakOut){return resolve('resolved');}
+				
+			for(let index=gIndex+gOffset,l=array.length; (loadedAllMessages || index<l-nBuffer) && index<l; index++, gIndex++){
+				console.log('calling callback with',index,gIndex)
+				let response = await callback(array[index], index, array, gIndex);
+				//console.log('got response',response);
+				if(typeof response == 'number'){
+					index=response-1
+					continue;
+				}
+				if(response){
+					breakOut=true;
+					return resolve('resolved');
+				}
+			};
+			
+			if(loadedAllMessages){
+				return resolve('resolved');
+			}
+
+
+			//test length and delete the beginning of the array to clean up
+			if(array.length>(nBuffer*2)+1){
+				let remove = array.length-((nBuffer*2)+1);
+				gOffset-= remove;  //array.length-(nBuffer)
+				array.splice(0,remove);	
+			}
+			
+		}
+
+		let _fetchMessages=function(resolve){
+			if(breakOut){return resolve('resolved');}
+			//https://discord.js.org/#/docs/main/master/class/MessageManager?scrollTo=fetch
+			channel.messages.fetch(opts,false,true).then(function(messages){ 
+				console.log('fetched messages',messages.length)
+				if(breakOut){return resolve('resolved');}
+				const messagesArray = messages.array();
+
+				if(!messagesArray.length){
+					loadedAllMessages=true;
+				}else{
+					array.push.apply(array,messagesArray);
+					opts.before = messagesArray[(messagesArray.length - 1)].id
+					_fetchMessages(resolve);
+				}
+				_processTick(resolve);
+			});
+		}
+		
+
+		return new Promise(resolve => {
+			console.log('started promise')
+			_fetchMessages(resolve);
+		});
+	}
 
 

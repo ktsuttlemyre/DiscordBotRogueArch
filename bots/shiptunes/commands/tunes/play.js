f192180c93252ed5680011a0608330422bee9c9a
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-22 22:37:56 -0400

    Update play.js

--- bots/shiptunes/commands/tunes/play.js
+++ bots/shiptunes/commands/tunes/play.js
@@ -46,6 +46,11 @@ class CustomCommand extends Command {
 	}
 
 	userPermissions(message) {
+		//simple playlist filter
+		//youtube uses list= as a url param and spotify uses /playlist/ endpoint
+		if(message.content.includes('list=') || message.content.includes('/playlist/'){
+		   return 'DJ';
+		}
 		return util.player.commandPermissions(message);
 	}
 	

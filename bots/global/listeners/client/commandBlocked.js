4d5b28aeebe70d484cb7a7410cc25e8ba4755f52
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-03-22 22:06:13 -0400

    Update commandBlocked.js

--- bots/global/listeners/client/commandBlocked.js
+++ bots/global/listeners/client/commandBlocked.js
@@ -1,5 +1,5 @@
 const { Listener } = require('discord-akairo');
-const commandVars = require.main.require.commandVars(__filename);
+const commandVars = require.main.require('./common').commandVars(__filename);
 
 class CommandBlockedListener extends Listener {
     constructor() {

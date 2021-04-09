fe4071a8277317008146c58d974a81898589f03c
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-04-09 14:12:16 -0400

    Update index.js

--- sounds/index.js
+++ sounds/index.js
@@ -1,5 +1,9 @@
 module.exports={
-  "125821419308318720":"seinfeld", //sinfeld.mp3
-  "217497619537068032":"nickelodeon", //nickelodeon
-  "692186689946255490":"nickelodeon",
+  "125821419308318720":"seinfeld", //wholius
+  "217497619537068032":"nickelodeon", //nick monger
+  "692186689946255490":"nickelodeon", //nick monger2
+  "146013495153000449":"whassup_dude",
+  "":"",
+  "500468522468507648","WhoCanItBeNow", //shipwash
+  "500467960914116609","WhoCanItBeNow" //streamwash
 }

d3a8b4b8d623dd3359b33ed42298d01aa79a4489
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-04-09 14:16:18 -0400

    Update index.js

--- sounds/index.js
+++ sounds/index.js
@@ -1,10 +1,10 @@
 module.exports={
   "default":"https://www.youtube.com/watch?v=QFewaD67FEY",
-  "125821419308318720":"seinfeld", //wholius
-  "217497619537068032":"nickelodeon", //nick monger
-  "692186689946255490":"nickelodeon", //nick monger2
-  "146013495153000449":"whassup_dude",
+  "125821419308318720":"seinfeld.mp3", //wholius
+  "217497619537068032":"nickelodeon.mp3", //nick monger
+  "692186689946255490":"nickelodeon.mp3", //nick monger2
+  "146013495153000449":"whassup_dude.mp3",
   "":"",
-  "500468522468507648","WhoCanItBeNow", //shipwash
-  "500467960914116609","WhoCanItBeNow" //streamwash
+  "500468522468507648","WhoCanItBeNow.mp3", //shipwash
+  "500467960914116609","WhoCanItBeNow.mp3" //streamwash
 }

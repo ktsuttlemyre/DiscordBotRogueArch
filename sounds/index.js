516e6e291c3c729e101174917cd0d00b7d925ec4
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-04-09 19:34:36 -0400

    Update index.js

--- sounds/index.js
+++ sounds/index.js
@@ -3,7 +3,7 @@ module.exports={
   "125821419308318720":"seinfeld.mp3", //wholius
   "217497619537068032":"nickelodeon.mp3", //nick monger
   "692186689946255490":"nickelodeon.mp3", //nick monger2
-  "146013495153000449":"whassup_dude.mp3", //waveofbabies newguy
+  "146013495153000449":"wassup_dude.mp3", //waveofbabies newguy
   "171438444466274306":"Caw.mp3", //munk
   "":"",
   "500468522468507648":"WhoCanItBeNow.mp3", //shipwash

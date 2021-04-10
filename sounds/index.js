de40916d5d3590d1cfaffd013cd7203ec87371a9
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-04-09 22:41:41 -0400

    Update index.js

--- sounds/index.js
+++ sounds/index.js
@@ -5,6 +5,7 @@ module.exports={
   "692186689946255490":"nickelodeon.mp3", //nick monger2
   "146013495153000449":"wassup_dude.mp3", //waveofbabies newguy
   "171438444466274306":"Caw.mp3", //munk
+  "759895781708136448":"Poots_on_Newts-Adventure_Time.mp3",
   "":"",
   "500468522468507648":"WhoCanItBeNow.mp3", //shipwash
   "500467960914116609":"WhoCanItBeNow.mp3" //streamwash

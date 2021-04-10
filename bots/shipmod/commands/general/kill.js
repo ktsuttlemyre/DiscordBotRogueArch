22539a1ad0252f24fbedfdfc85785d6f74a8219c
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-04-10 18:47:49 -0400

    Update kill.js

--- bots/shipmod/commands/general/kill.js
+++ bots/shipmod/commands/general/kill.js
@@ -1,18 +1,15 @@
-const GUIMessages = require.main.require('./templates/messages');
+
 const { Command } = require('discord-akairo');
-const { Player } = require("discord-player");
 const emotes={error:":error:"}
-const {reactions,defaultAvatar} = require.main.require('./common');
-const common = require.main.require('./common');
-const _ = require('lodash');
-const path = require('path');
+const util = require.main.require('./util');
+const commandVars = util.commandVars(__filename);
 
 class CustomCommand extends Command {
 	constructor() {
-		super(path.parse(__filename).name, {
+		super(commandVars.name, {
 		description: { content: 'kill'},
-		aliases: ['kill'],
-		category: path.basename(path.dirname(__filename)),
+		aliases: [commandVars.name],
+		category: commandVars.category,
 		clientPermissions: ['SEND_MESSAGES'],
 		args: [
 			// {

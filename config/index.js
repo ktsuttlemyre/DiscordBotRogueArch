12beb52da22306d60042d279578d364bf7d44c88
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-04-15 20:59:23 -0400

    Update index.js

--- config/index.js
+++ config/index.js
@@ -23,9 +23,26 @@ let defaults={
 		resetUserState:true,
 		tones:{
 			on:true,
-			defaultJoinTone:"", //currently not used
-			defaultLeaveTone:'https://youtu.be/xk093ODaNjc',
+			defaultJoinTone: "https://www.youtube.com/watch?v=RtCt5gKMBMw", //"https://www.youtube.com/watch?v=QFewaD67FEY", //currently not used
+			defaultLeaveTone: "https://www.youtube.com/watch?v=ea44CKOKlP8", //'https://youtu.be/xk093ODaNjc',
 			location: 'https://shipwa.sh/discord/tones/',//'http://s3.amazonaws.com/shiptunes/tones/', //'./sounds/'
+			enableCustom:true,
+			custom:{
+			  "125821419308318720":"seinfeld.mp3", //wholius
+			  "217497619537068032":"nickelodeon.mp3", //nick monger
+			  "692186689946255490":"nickelodeon.mp3", //nick monger2
+			  "146013495153000449":"wassup_dude.mp3", //waveofbabies newguy
+			  "171438444466274306":"Caw.mp3", //munk
+			  "759895781708136448":"Poots_on_Newts-Adventure_Time.mp3",
+			  "679077100707381263":"Gummys_Theme.mp3", //Gummy
+			  "489174943200837632":"Team_Fortress_2_Soundtrack_-_Ending_Flourish.mp3", //crowsair
+			  "141026440782020608":"Show_me_that_smile.mp3", //troy
+			  "218890918764347393":"TRUMPETS.mp3", //enzan
+			  "337441260992200706":["garzon1.mp3":"garzon2.mp3"], //garzon
+			  "":"",
+			  "500468522468507648":"WhoCanItBeNow.mp3", //shipwash
+			  "500467960914116609":"WhoCanItBeNow.mp3" //streamwash
+			}
 		}	
 	},
 	encapsulateAdminRoles:'Admin',

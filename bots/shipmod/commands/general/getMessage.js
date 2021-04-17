cdc24a5098493082cca5047e8cf3e921df320ffc
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-04-17 19:09:23 -0400

    Create getMessage.js

---
+++ bots/shipmod/commands/general/getMessage.js
@@ -0,0 +1,54 @@
+const GUIMessages = require.main.require('./templates/messages');
+const { Command } = require('discord-akairo');
+const { Player } = require("discord-player");
+const emotes={error:":error:"}
+const {reactions,defaultAvatar} = require.main.require('./common');
+const common = require.main.require('./common');
+const _ = require('lodash');
+const path = require('path');
+const util= require.main.require('./util');
+
+class CustomCommand extends Command {
+	constructor() {
+		super(path.parse(__filename).name, {
+		description: { content: 'fetchall'},
+		aliases: ['fetchall'],
+		category: path.basename(path.dirname(__filename)),
+		clientPermissions: ['SEND_MESSAGES'],
+		args: [
+			{
+				id: 'search',
+				default: '',
+				match: 'content',
+			},
+			],
+		channelRestriction: 'guild', 
+		});
+	}
+	
+	userPermissions(message) {
+		if (!message.member.roles.cache.some(role => role.name === 'Admin')) {
+			return 'Admin';
+		}
+		return null;
+	}
+
+	async exec(message, {search}) {
+
+	    //let messages = await common.getMessages(message.channel);
+	    //message.channel.send(`got ${messages.length} messages`);
+		
+		let id = search
+	await common.fetchMessages(message.channel,function(message,index,messages,gIndex){
+		if(message.id == search){
+      return 
+    }
+	})
+
+ message.channel.send('```'+JSON.stringify(message)+'```')
+
+    
+	}
+}
+
+module.exports = CustomCommand;

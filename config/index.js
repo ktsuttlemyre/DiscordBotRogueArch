90c43f4681a05791ffc317ff83ce741b7da098ab
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-04-18 15:19:55 -0400

    Update index.js

--- config/index.js
+++ config/index.js
@@ -31,24 +31,7 @@ let defaults={
 			externalMap: 'https://shipwa.sh/discord/tones/index.js',
 			externalMapUpdateInterval:0,
 			enableCustom:true,
-			custom:{
-			  "125821419308318720":"seinfeld.mp3", //wholius
-			  "217497619537068032":"nickelodeon.mp3", //nick monger
-			  "692186689946255490":"nickelodeon.mp3", //nick monger2
-			  "146013495153000449":"wassup_dude.mp3", //waveofbabies newguy
-			  "171438444466274306":"Caw.mp3", //munk
-			  "759895781708136448":"Poots_on_Newts-Adventure_Time.mp3",
-			  "679077100707381263":"Gummys_Theme.mp3", //Gummy
-			  "489174943200837632":"Team_Fortress_2_Soundtrack_-_Ending_Flourish.mp3", //crowsair
-			  "141026440782020608":"Show_me_that_smile.mp3", //troy
-			  "218890918764347393":"TRUMPETS.mp3", //enzan
-			  "337441260992200706":["garzon1.mp3","garzon2.mp3"], //garzon
-			  "116021432894357511":"shipmod-joelseph.mp3", //joel
-			  "137618156989579264":"https://www.youtube.com/watch?v=O6ksGHgQa0s",
-			  "":"",
-			  "500468522468507648":"WhoCanItBeNow.mp3", //shipwash
-			  "500467960914116609":"https://www.youtube.com/watch?v=P6CzzynlYuY" //streamwash
-			}
+			custom:{}
 		}	
 	},
 	encapsulateAdminRoles:'Admin',

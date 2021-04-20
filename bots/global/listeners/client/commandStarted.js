248dfdeb303d1556d07f79d29639a38fbc07fe40
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-04-20 13:56:13 -0400

    Update commandStarted.js

--- bots/global/listeners/client/commandStarted.js
+++ bots/global/listeners/client/commandStarted.js
@@ -1,24 +1,24 @@
 const { Listener } = require('discord-akairo');
-const {reactions,defaultAvatar} = require.main.require('./common');
-const commandVars = require.main.require('./util').commandVars(__filename);
+const util = require.main.require('./util');
+const config = util.config;
+const commandVars = util.commandVars(__filename);
 
 // https://discord-akairo.github.io/#/docs/main/master/class/CommandHandler?scrollTo=e-commandStarted
-class CommandBlockedListener extends Listener {
-    constructor() {
-        super('global/'+commandVars.id, {
-            emitter: 'commandHandler',
-            event: 'commandStarted'
-        });
-    }
+class CustomListener extends Listener {
+	constructor() {
+		super('global/'+commandVars.id, {
+			emitter: commandVars.category,
+			event: commandVars.name,
+			category: commandVars.category,
+		});
+	}
 
-    exec(message, command, args) {
-        message.react(reactions.shipwash); //THIS should be handled elsewhere
-        //console.log(`${message.author.username} was blocked from using ${command.id} because of ${reason}!`);
-            
+
+    exec(message, command, args) {            
         this.client.memory.channelSet(message,`${message.id}_promise`,new Promise(resolve => {
             this.client.memory.channelSet(message,`${message.id}_resolve`,resolve);
         }));
     }
 }
 
-module.exports = CommandBlockedListener;
+module.exports = CustomListener;

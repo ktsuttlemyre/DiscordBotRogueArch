cdb65f5b850058ceab5a7d481a1a6e7d8d93fe24
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-04-25 17:00:28 -0400

    Update presenceUpdate.js

--- bots/shipmod/listeners/client/presenceUpdate.js
+++ bots/shipmod/listeners/client/presenceUpdate.js
@@ -33,47 +33,44 @@ class CustomListener extends Listener {
 			return
 		}
     
-    let member = newPresence.member || newPresence.user
-    let user = newPresence.user
-    let client = this.client;
-   
-    if(user.bot){ //ignore bots
-      return
-    }
-    
-		//var env = process.env.ENVIRONMENT
-    //if(env != 'production'){ //only work on production
-		//	return;
-		//}
-    
-		/** Information gathering **/
-		//make sure message is resolved
-		let message = await util.messages.resolve(reaction.message);
+	    let member = newPresence.member || newPresence.user
+	    let user = newPresence.user
+	    let client = this.client;
 
-		let name = member.displayName || member.username || member.tag;
-		let userID = member.id || member.user.id;
-    
-    let presence = newPresence.activities.filter(x=>x.type === "PLAYING") //outputs the presence which the user is playing
-    let game = presence[0].name // will give the name of the game 
-    let roleName = `🎮${game}`
-    
-    //if the role doesn't exist make it
-    let role = await message.guild.roles.create({
-        data:{
-          name: roleName,
-          color: "DEFAULT",
-          mentionable: true
-        },
-        reason:"Game Activity"
-    })
-    
-    //if the user doesn't have the role then add it
-    if (! member.roles.cache.some(role => role.name === roleName)) {
-      member.roles.add(role)
-    }
-    
-    //log
-    
+	    if(user.bot){ //ignore bots
+	      return
+	    }
+
+			//var env = process.env.ENVIRONMENT
+	    //if(env != 'production'){ //only work on production
+			//	return;
+			//}
+
+	    /** Information gathering **/
+	    let name = member.displayName || member.username || member.tag;
+	    let userID = member.id || member.user.id;
+
+	    let presence = newPresence.activities.filter(x=>x.type === "PLAYING") //outputs the presence which the user is playing
+	    let game = presence[0].name // will give the name of the game 
+	    let roleName = `🎮${game}`
+
+	    //if the role doesn't exist make it
+	    let role = await message.guild.roles.create({
+		data:{
+		  name: roleName,
+		  color: "DEFAULT",
+		  mentionable: true
+		},
+		reason:"Game Activity"
+	    })
+
+	    //if the user doesn't have the role then add it
+	    if (! member.roles.cache.some(role => role.name === roleName)) {
+	      member.roles.add(role)
+	    }
+
+	    //log
+
 	}
 }
 module.exports = CustomListener;

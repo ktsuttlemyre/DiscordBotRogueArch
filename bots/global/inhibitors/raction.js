ee91a7ee2c5a267dd8571f784ec36ca4a2887ca0
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-05-05 21:30:52 -0400

    Create raction.js

---
+++ bots/global/inhibitors/raction.js
@@ -0,0 +1,35 @@
+const {Inhibitor} = require("discord-akairo");
+const util = require.main.require('./util');
+const commandVars = util.commandVars(__filename);
+const common = require.main.require('./common')
+const reactions = common.reactions
+
+class CustomInhibitor extends Inhibitor {
+	constructor() {
+		super(commandVars.name, {
+			reason: "command already claimed by other bot instance",
+			type: "all",
+		});
+	}
+
+	async exec(message) {
+    //https://stackoverflow.com/questions/59988625/discord-js-fetchmessage-reactions-get-cannot-get-users
+    
+//    let users = await message.reactions.cache.users.fetch()
+//      // I'm not quite sure what you were trying to accomplish with the original lines
+//   reaction.cache.map((item) => item.users.cache.array())
+    
+    
+    var reaction = message.reactions.get(reactions.shipwash)
+    reaction.fetchUsers();
+
+    for (const user of reaction.users.values()) {
+      const id = user.id;
+      if(id == this.client.user.id){
+        return true
+      }
+		return false;
+	}
+}
+
+module.exports = CustomInhibitor;

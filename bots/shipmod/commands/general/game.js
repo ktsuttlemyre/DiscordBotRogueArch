fbd40b91bcc824ba108e00146840e04cd8afcaf8
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-09-28 21:38:16 -0400

    Update game.js

--- bots/shipmod/commands/general/game.js
+++ bots/shipmod/commands/general/game.js
@@ -105,13 +105,12 @@ class CustomCommand extends Command {
 
 		}else if(keyword){
 			let mentions = await util.resolveMentions(message,keyword);
-			message.channel.send('got user:'+mentions.role)
 			if(mentions.role){
 				if(mentions.role.name.indexOf(gamePrefix)!=0){
 					return 'The requested role `'+mentions.role.name+'` is not a game role'
 				}
 				var array = mentions.role.members.map(m=>m.user.displayName||m.user.name||m.user.tag);
-				return 'Game `'+mentions.role.name+'` is played by \n`'+array.join('``')+'`'
+				return 'Game `'+mentions.role.name+'` is played by \n `'+array.join('` `')+'`'
 			}
 			
 		}else{

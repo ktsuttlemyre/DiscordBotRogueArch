aa9eb3ef8b1629a5bb7d84d2703249802bc348bb
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-09-28 20:14:08 -0400

    Update ready.js

--- bots/shipmod/listeners/client/ready.js
+++ bots/shipmod/listeners/client/ready.js
@@ -26,58 +26,58 @@ class CustomListener extends Listener {
 	
 
 	async exec() {
-		let client = this.client;
+// 		let client = this.client;
 
-		var env = process.env.ENVIRONMENT;
-		if (env != "production") {
-			return;
-		}
+// 		var env = process.env.ENVIRONMENT;
+// 		if (env != "production") {
+// 			return;
+// 		}
 
-		//trigger listeners
-		/* devnote
-		 * Loop through all guilds
-		 * loop through all channels we have permission to in those guilds
-		 * emit voice channel changes for voice-to-text channel linking
-		 * check if there are any commands that were not executed
-		 */
-		client.guilds.cache.forEach(async function (guild) {
-			//.get("690661623831986266"); // Getting the guild.
-			console.log("Custom Ready rutine running for ",client.user.tag," checking guild ", guild.name, guild.id);
+// 		//trigger listeners
+// 		/* devnote
+// 		 * Loop through all guilds
+// 		 * loop through all channels we have permission to in those guilds
+// 		 * emit voice channel changes for voice-to-text channel linking
+// 		 * check if there are any commands that were not executed
+// 		 */
+// 		client.guilds.cache.forEach(async function (guild) {
+// 			//.get("690661623831986266"); // Getting the guild.
+// 			console.log("Custom Ready rutine running for ",client.user.tag," checking guild ", guild.name, guild.id);
 
-			let logChannel = guild.channels.resolve(config.actionLogChannel);
-			let gameChannel = guild.channels.resolve(config.gameChannel);
-			if (logChannel && logChannel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
-				logChannel.send(`${client.user.tag} woke up`);
-			}
+// 			let logChannel = guild.channels.resolve(config.actionLogChannel);
+// 			let gameChannel = guild.channels.resolve(config.gameChannel);
+// 			if (logChannel && logChannel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+// 				logChannel.send(`${client.user.tag} woke up`);
+// 			}
 			
-			let gamePrefix="🎮";
+// 			let gamePrefix="🎮";
 
-			let gameRoles = guild.roles.cache.filter((x) => x.name.indexOf(gamePrefix)===0); //find a role with game prefix
-			console.log('gameRoles found =',gameRoles.size)
-			if(gameRoles.size){
-				gameRoles = gameRoles.sorted(sortAlphaNum)
+// 			let gameRoles = guild.roles.cache.filter((x) => x.name.indexOf(gamePrefix)===0); //find a role with game prefix
+// 			console.log('gameRoles found =',gameRoles.size)
+// 			if(gameRoles.size){
+// 				gameRoles = gameRoles.sorted(sortAlphaNum)
 
 
-				let games = Array.from(gameRoles, ([name, value]) => ({ game:value.name, members:mapToArray(value.members) }));
-				//let json = JSON.stringify(games,null,2)
-				let data = YAML.dump(games,{noArrayIndent :true,flowLevel:1,sortKeys:true,forceQuotes:true,quotingType:'"'}) //https://www.npmjs.com/package/js-yaml
-				console.log(data)
-				data=data.replace(/\-\s\{\"game\":\s/g,"```").replace(/,\s\"members\":\s\[/g,'\n Players: ').replace(/\]\}/g,'```') .replace(/"/g,'')
+// 				let games = Array.from(gameRoles, ([name, value]) => ({ game:value.name, members:mapToArray(value.members) }));
+// 				//let json = JSON.stringify(games,null,2)
+// 				let data = YAML.dump(games,{noArrayIndent :true,flowLevel:1,sortKeys:true,forceQuotes:true,quotingType:'"'}) //https://www.npmjs.com/package/js-yaml
+// 				console.log(data)
+// 				data=data.replace(/\-\s\{\"game\":\s/g,"```").replace(/,\s\"members\":\s\[/g,'\n Players: ').replace(/\]\}/g,'```') .replace(/"/g,'')
 
-				if(gameChannel && gameChannel.permissionsFor(guild.me).has("SEND_MESSAGES")){
-					Discord.Util.splitMessage(data,{maxLength:1900}).forEach(function(mess){
-						gameChannel.send(mess);
-					})
-				}
+// 				if(gameChannel && gameChannel.permissionsFor(guild.me).has("SEND_MESSAGES")){
+// 					Discord.Util.splitMessage(data,{maxLength:1900}).forEach(function(mess){
+// 						gameChannel.send(mess);
+// 					})
+// 				}
 				
-			}
+// 			}
 			
 			
-		}); //end guilds
+// 		}); //end guilds
 		
 
 
-		console.log(`Custom Ready rutine is complete for ${client.user.tag}`);
+// 		console.log(`Custom Ready rutine is complete for ${client.user.tag}`);
 	} //end exec
 }
 

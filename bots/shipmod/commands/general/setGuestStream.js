e9793c73290e3d437f5ed60d63146aa05f452780
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-09-29 22:27:47 -0400

    Update setGuestStream.js

--- bots/shipmod/commands/general/setGuestStream.js
+++ bots/shipmod/commands/general/setGuestStream.js
@@ -44,8 +44,7 @@ class CustomCommand extends Command {
 		memory.set(message,'gueststreamgame',mentions.args[1])
     		return {
 			title:mentions.content,
-			description:"set guest to: "+user.displayName||user.tag+'\n'+
-			'set game to:'+mentions.args[1]+'\n',
+			description:"set guest to: "+user.displayName||user.tag+'\nset game to:'+mentions.args[1]+'\n',
 			thumbnail: {
 			      "url": (user.user||user).displayAvatarURL()
 			    }

475cad754328683540d10274dd43ef6e84080e25
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-09-29 23:05:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -299,11 +299,11 @@ function shutdown(signal) {
 			guest=guest.user||guest;
 			let string = '<html><head>'+
 				'<style>html,body,img{margin:0;border:0;width:100%;height:100%}'+
-				'img{border-radius: 50%;width:auto !important;}'+
+				'img{border-radius: 50%;width:auto !important;float:left}'+
 				'body{font-family: Arial, Helvetica, sans-serif;font-weight: bold;font-size:2em;color: white;-webkit-text-stroke-width: 2px;-webkit-text-stroke-color: black;}'+
 				'</style></head><body><img src="'+guest.displayAvatarURL()+'">'
 			if(game){
-				string+='&nbsp;'+guest.displayName||guest.tag+'is playing:<br>&nbsp;'+game
+				string+='&nbsp;'+(guest.displayName||guest.tag)+'is playing:<br>&nbsp;'+game
 			}
 			string+='</body></html>'
 			

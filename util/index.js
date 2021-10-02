bb781402ab010a2f9d2a07f415da75dc2922072f
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-10-01 21:27:10 -0400

    Update index.js

--- util/index.js
+++ util/index.js
@@ -311,7 +311,7 @@ let promiseResolve = async function(array,fn){
 }
 module.exports.resolveMentions = async function(message,string,opts){
 		opts = opts || {};
-		let allowInnerRawIDs = (opts.allowInnerRawIDs!==undefined)?opts.allowInnerRawIDs:true;
+		let allowInnerRawIDs = (opts.allowInnerRawIDs===undefined)?true:opts.allowInnerRawIDs;
 		let guild = message.guild
 		//test string
 		//"I think we should add <@86890631690977280> to the <@&134362454976102401> role for the channel <#222197033908436994> and here is a random nickname <@!86890631690977280>".replace(/<(@|@!|#|@&)(\d+)>|(\d+)/,function(match,g1,g2,g3){

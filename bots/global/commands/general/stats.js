6604841cd515aa844e942702b8e1d886fbae1bbc
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-10-10 18:55:02 -0400

    Update stats.js

--- bots/global/commands/general/stats.js
+++ bots/global/commands/general/stats.js
@@ -1,6 +1,7 @@
 const {Command, version: akairoVersion} = require("discord-akairo");
 const {MessageEmbed, version: djsVersion} = require("discord.js");
 const {version: botVersion} = require.main.require("./package.json");
+const osu = require('node-os-utils');
 
 class StatsCommand extends Command {
 	constructor() {
@@ -32,13 +33,34 @@ class StatsCommand extends Command {
 		return `${days === "00" ? "" : `${days}:`}${hours}:${minutes}:${seconds}`;
 	}
 
-	async exec(message) {
+	async exec(message) {		
+		//returns cpu average and count
+		let cpu = osu.cpu
+		let cpuAverage = cpu.average()
+		let cpuCount = cpu.count()
+		//returns cpu usage
+		//let usage = await cpu.usage();
+
+		//returns drive info
+		//let driveInfo = await osu.drive.info();
+		//returns memory info
+		let mem = osu.mem
+		//let memInfo = await mem.info()
+		
+		let res = await Promise.all([cpu.usage(),osu.drive.info(),mem.info()]);
+		let cpuUsage = res[0];
+		let driveInfo = res[1];
+		let memInfo = res[2];
+		
+		
+		const client = this.client;
+		
 		const embed = new MessageEmbed()
 			.setColor(0xffac33)
 			.setTitle("Akairo Boilerplate Statistics")
 			.addField(
 				"Discord",
-				[`**Guilds**: ${this.client.guilds.size}`, `**Channels**: ${this.client.channels.size}`, `**Users**: ${this.client.users.size}`],
+				[`**Guilds**: ${client.guilds.cache.size}`, `**Channels**: ${client.channels.cache.size}`, `**Users**: ${client.users.cache.size}`],
 				true
 			)
 			.addField(
@@ -51,9 +73,19 @@ class StatsCommand extends Command {
 				],
 				true
 			)
-			.setFooter("Akairo Boilerplate v" + botVersion);
+			.addField(
+				"Technical2",
+				[
+					`**cpu usage**: JSON.stringify(cpuUsage),
+					`**diskinfo**: JSON.st,ringify(disKInfo),
+					`**Discord.js**: v${djsVersion}`
+					`**Akairo**: v${akairoVersion}`,
+				],
+				true
+			)
+			.setFooter("Shipbot v" + botVersion);
 
-		return message.channel.send(embed);
+		return embed;
 	}
 }
 

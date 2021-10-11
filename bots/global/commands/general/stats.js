00a90f9eb75db016cb1e223669d5883f97c606c9
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-10-11 00:50:40 -0400

    Update stats.js

--- bots/global/commands/general/stats.js
+++ bots/global/commands/general/stats.js
@@ -91,8 +91,9 @@ class StatsCommand extends Command {
 		let driveInfo = res[1];
 		let memInfo = res[2];
 		
-		let dep = await (getVersions().stdout || '')
-		let list = dep.replace(/\+\-\-/ig,'').replace(/\`\-\-/ig,'').split('\n')
+		let dep = await getVersions()
+		dep = ((dep || {}).stdout || '')
+		let list = dep.replace(/\+\-\-/ig,'').replace(/\`\-\-/ig,'').trim().split('\n')
 		const half = Math.ceil(list.length / 2);    
 
 		const firstHalf = list.slice(0, half)

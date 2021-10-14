cc48845c5b10ee3adb03dfff9399b4e150e2ab0f
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-10-14 16:51:22 -0400

    Update messageReactionAdd.js

--- bots/shipmod/listeners/client/messageReactionAdd.js
+++ bots/shipmod/listeners/client/messageReactionAdd.js
@@ -59,8 +59,8 @@ class CustomListener extends Listener {
 
 		/** Emoji alerts **/
 		let key = `${message.id}/${userID}`;
-		let cache = this.client.memory.get(message.channel, "reactionCache", {});
-		let cacheFunctions = this.client.memory.get(message.channel, "reactionListener", {});
+		let cache = this.client.memory.channelGet(message, "reactionCache", {});
+		let cacheFunctions = this.client.memory.channelGet(message, "reactionListener", {});
 
 		let entry = cache[key] || (cache[key] = []);
 		entry.push(reaction);

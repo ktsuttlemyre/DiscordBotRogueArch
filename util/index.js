49be98f0641c11aa0154f277388a56da528a6672
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-10-14 03:05:38 -0400

    Update index.js

--- util/index.js
+++ util/index.js
@@ -120,7 +120,7 @@ module.exports.parseSettingsFromGuild = async function (guild, channel){
 	    `The guild owner \`${owner.username || owner.tag}\` must be present\n`+
 	    `Only valid YAML messages created by \`owner\` or by messages that are 👍 reacted by owner will be accepted\n`+
 	    `You may create multiple config messages that will be merged in chronological order (To circumvent discord's 2k message length)\n`+
-	    `You are allocated ${upperCharacterLimit/1000}kb of parsed config space`+
+	    `You are allocated ${upperCharacterLimit/1000}kb of parsed config space`;
 	    //`\`\`\``
 	
 	

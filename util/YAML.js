5d7b4c87774cac673d47db0a40f9a9d149733a05
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-10-16 12:30:28 -0400

    Update YAML.js

--- util/YAML.js
+++ util/YAML.js
@@ -1,17 +1,19 @@
 
 const YAML = require("js-yaml");
 const UTIL = require('util');
+const _ = require("lodash");
 
-YAML.stringify = function(obj){
-	//let json = JSON.parse(UTIL.inspect(obj, {showHidden: false}))
-	return YAML.dump(obj,{
+YAML.stringify = function(obj,opts){
+	opts = _.defaults(opts||{},{
 		noArrayIndent:true,
 		flowLevel:1,
 		sortKeys:true,
 		forceQuotes:true,
 		quotingType:'"',
 		skipInvalid:true,
-	}) //https://www.npmjs.com/package/js-yaml
+	})
+	//let json = JSON.parse(UTIL.inspect(obj, {showHidden: false}))
+	return YAML.dump(obj,opts) //https://www.npmjs.com/package/js-yaml
 }
 
 module.exports = YAML

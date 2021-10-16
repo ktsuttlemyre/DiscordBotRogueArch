01a6fdd15723dcf7ab460dd11ecfd08bbd405097
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-10-16 11:29:36 -0400

    Create YAML.js

---
+++ util/YAML.js
@@ -0,0 +1,7 @@
+
+const YAML = require("js-yaml");
+YAML.stringify = function(yaml){
+	return YAML.dump(yaml,{noArrayIndent :true,flowLevel:1,sortKeys:true,forceQuotes:true,quotingType:'"'}) //https://www.npmjs.com/package/js-yaml
+}
+
+module.exports.YAML = YAML
